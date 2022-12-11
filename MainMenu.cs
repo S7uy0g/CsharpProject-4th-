5bbd213a650db346cafa684d12159edbd38933aa
AniruDra <magarrudra999@gmail.com>
2022-12-11 08:55:39 +0545

    Main Menu made

---
+++ MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CsharpForm
+{
+    public partial class MainMenu : Form
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+        //Main Menu main Form
+        private void MainMenu_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
